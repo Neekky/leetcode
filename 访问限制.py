cc705589787827582fcdc0b2d166bc5d188da737
Neekky <475383200@qq.com>
2018-09-19 15:49:06 +0800

    练习题：定义能访问类的私有变量的函数

---
+++ 访问限制.py
@@ -0,0 +1,18 @@
+class student(object):
+    def __init__(self, name, gender):
+        self.__name = name
+        self.__gender = gender
+    def get_gender(self):
+        return self.__gender
+    def set_gender(self,gender):
+        self.__gender = gender
+
+bart = student('Bart', 'male')
+if bart.get_gender() != 'male':
+    print('测试失败!')
+else:
+    bart.set_gender('female')
+    if bart.get_gender() != 'female':
+        print('测试失败!')
+    else:
+        print('测试成功!')
